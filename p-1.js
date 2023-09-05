c91be159cd710c33715a6ffb057640ce09b75072
th <tanvirhossen808@gmail.com>
2023-09-05 10:15:07 +0600

    first cdommit

---
+++ p-1.js
@@ -0,0 +1,28 @@
+// function usingFunction() {
+//     document.getElementById('inputMethod').innerText = 'funcion set';
+// }
+// document.getElementById('direct').addEventListener('click', function () {
+//     document.getElementById('inputMethod').innerText = 'direct set';
+// })
+// document.getElementById('update').addEventListener('click', function () {
+//     const value1 = document.getElementById('value');
+//     const p = document.getElementById('inputMethod');
+//     p.innerText = value1.value;
+//     value1.value = '';
+
+// })
+
+// 2
+
+// document.getElementById('post').addEventListener('click', function () {
+//     console.log('c')
+
+//     const post = document.getElementById('div1');
+//     const input1 = document.getElementById('addComment');
+//     const p = document.createElement('p');
+//     p.innerText = input1.value;
+
+//     post.appendChild(p);
+//     input1.value = '';
+
+// })
